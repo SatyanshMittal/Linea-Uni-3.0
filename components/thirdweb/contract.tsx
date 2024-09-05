5fdea7de40a8c95496ae4d9e6906b9224c4f9ca4
Satyansh Mittal <mittalsatyansh@gmail.com>
2024-09-06 02:33:36 +0530

    added linea chain

--- components/thirdweb/contract.tsx
+++ components/thirdweb/contract.tsx
@@ -11,7 +11,7 @@ export const client = createThirdwebClient({
 // connect to your contract
 export const contract = getContract({ 
   client, 
-  chain: defineChain(656476), 
+  chain: defineChain(59141), 
   // address: "0x6AC0AC64D21ecE4CD8d139DD362172C3C50b2C96"
-  address:  "0xc12026749E1A1382413B785cCa0eBb55142c8d52"
+  address:  "0x97B6F9bcF98fd7aD37355CB99016fF644Bef034D"
 });
